484c04686cc57815323a3e283fbe0f949fef6b98
itsniteshcal <62937703+itsniteshcal@users.noreply.github.com>
2023-10-27 22:30:54 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+package opps_Concept;
+
+import abstraction.abstractclass.Sample1;
+
+public class Mainclass1 {
+	public static void main(String[] args) {
+		Sample2 ref = new Sample2();
+		ref.demo();
+		ref.disp();
+	}
+
+}
